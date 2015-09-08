24cf41e8ef69f55f2cced7ed4333356d176be7c4
e2tha-e <hyunhoee@gmail.com>
2015-09-08 17:50:17 -0400

    upgrading to pln 0.11.0, and starting with fresh install

--- patternlab-node/public/static/js/fepper-configured.js
+++
@@ -1,6 +0,0 @@
-if (window.location.pathname.indexOf('/styleguide/html/styleguide.html') > -1 && window.location.search === '') {
-  window.location = '../../patterns/04-pages-00-homepage/04-pages-00-homepage.html';
-}
-//<![CDATA[
-  document.write('<script type="text/javascript" src="http://HOST:35729/livereload.js"><\/script>'.replace('HOST', location.hostname));
-//]]>
