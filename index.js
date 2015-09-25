3137e281e3352ab52d625ac0798e1023efe17f02
e2tha-e <hyunhoee@gmail.com>
2015-09-25 09:57:26 -0400

    indexes

--- index.js
+++ index.js
@@ -7,8 +7,5 @@
   var argv2 = process.argv[2] ? process.argv[2] : 'default';
   var utils = require('./fepper/lib/utils');
 
-  cp.spawn('./node_modules/.bin/gulp', [argv2], {stdio:'inherit'})
-    .on('exit', function (error) {
-      utils.error(error);
-    });
+  cp.spawn('./node_modules/.bin/gulp', [argv2], {stdio:'inherit'});
 })();
