4944532a79a246fa1a3c2b8f6935a3c8a64cb171
e2tha-e <hyunhoee@gmail.com>
2015-09-26 10:52:13 -0400

    stylus and scss

--- gulp/custom/css-process.js
+++ gulp/custom/css-process.js
@@ -7,20 +7,25 @@ var utils = require('../lib/utils');
 
 gulp.task('css-process', function () {
 // Uncomment if you want to use Stylus.
-  return gulp.src('./' + conf.src + '/css-processors/stylus/*.styl')
-    .pipe(plugins.stylus({linenos: true}))
-    .on('error', utils.handleError)
-    .pipe(gulp.dest('./' + conf.src + '/css'));
+//  return gulp.src('./' + conf.src + '/css-processors/stylus/*.styl')
+//    .pipe(plugins.stylus({
+//      linenos: true
+//    }))
+//    .on('error', utils.handleError)
+//    .pipe(gulp.dest('./' + conf.src + '/css'));
 // Uncomment if you want to use SCSS. Replace if you want to use something else.
 //  return gulp.src('./' + conf.src + '/css-processors/scss/*.scss')
-//    .pipe(plugins.sass({outputStyle: 'expanded'})
-//    .on('error', plugins.sass.logError))
+//    .pipe(plugins.sass({
+//      outputStyle: 'expanded',
+//      sourceComments: true
+//    }))
+//    .on('error', plugins.sass.logError)
 //    .pipe(gulp.dest('./' + conf.src + '/css'));
 });
  
 gulp.task('css-process:watch', function () {
 // Uncomment if you want to use Stylus.
-  gulp.watch('./' + conf.src + '/css-processors/stylus/**/*.styl', ['css-process']);
+//  gulp.watch('./' + conf.src + '/css-processors/stylus/**/*.styl', ['css-process']);
 // Uncomment if you want to use SCSS. Replace if you want to use something else.
 //  gulp.watch('./' + conf.src + '/css-processors/scss/**/*.scss', ['css-process']);
 });
