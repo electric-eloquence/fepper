e56f9e9f1fc2f4d609ceeb6621408d7f1258ac05
e2tha-e <hyunhoee@gmail.com>
2015-09-26 22:43:35 -0400

    path fix

--- gulp/fepper/fepper.js
+++ gulp/fepper/fepper.js
@@ -25,28 +25,28 @@
 
   gulp.task('fepper:copy-css', function () {
     if (typeof conf.backend.synced_dirs.css_dir === 'string' && conf.backend.synced_dirs.css_dir.match(/^[\w.\/-]+$/)) {
-      return gulp.src('./' + conf.src + '/css/*')
+      return gulp.src(rootDir + '/' + conf.src + '/css/*')
         .pipe(gulp.dest('backend/' + conf.backend.synced_dirs.css_dir));
     }
   });
 
   gulp.task('fepper:copy-fonts', function () {
     if (typeof conf.backend.synced_dirs.fonts_dir === 'string' && conf.backend.synced_dirs.fonts_dir.match(/^[\w.\/-]+$/)) {
-      return gulp.src('./' + conf.src + '/fonts/*')
+      return gulp.src(rootDir + '/' + conf.src + '/fonts/*')
         .pipe(gulp.dest('backend/' + conf.backend.synced_dirs.fonts_dir));
     }
   });
 
   gulp.task('fepper:copy-images', function () {
     if (typeof conf.backend.synced_dirs.images_dir === 'string' && conf.backend.synced_dirs.images_dir.match(/^[\w.\/-]+$/)) {
-      return gulp.src('./' + conf.src + '/images/*')
+      return gulp.src(rootDir + '/' + conf.src + '/images/*')
         .pipe(gulp.dest('backend/' + conf.backend.synced_dirs.images_dir));
     }
   });
 
   gulp.task('fepper:copy-js', function () {
     if (typeof conf.backend.synced_dirs.js_dir === 'string' && conf.backend.synced_dirs.js_dir.match(/^[\w.\/-]+$/)) {
-      return gulp.src('./' + conf.src + '/js/*/**/*')
+      return gulp.src(rootDir + '/' + conf.src + '/js/*/**/*')
         .pipe(gulp.dest('backend/' + conf.backend.synced_dirs.js_dir));
     }
   });
@@ -57,7 +57,7 @@
   });
 
   gulp.task('fepper:json-compile', function (cb) {
-    fepper.jsonCompile(conf.src);
+    fepper.jsonCompile(rootDir + '/' + conf.src);
     cb();
   });
 
