c474310a1d1f9c49b6a9789d06fe372b22c5d29b
e2tha-e <hyunhoee@gmail.com>
2015-10-15 11:58:59 -0400

    consistent quotes

--- gulp/contrib/multisite.js
+++ gulp/contrib/multisite.js
@@ -35,9 +35,9 @@
       // Begin backticked multi-line string.
       plOverriderContent += `
 (function multisite_` + version + ` () {
-  'use strict';
+  "use strict";
 
-  var sgNavContainer = document.getElementById('sg-nav-container');
+  var sgNavContainer = document.getElementById("sg-nav-container");
 `;    // End backticked multi-line string.
       // ///////////////////////////////////////////////////////////////////////
 
@@ -100,10 +100,10 @@
       var addressReplacement = (window.location.protocol == "file:") ? null : window.location.protocol+"//"+window.location.host+window.location.pathname.replace("index.html","")+"?p="+pattern;
       var href;
       var sgViewportPathname = document.getElementById("sg-viewport").contentWindow.location.pathname;
-      var pathParts = sgViewportPathname.split('/');
+      var pathParts = sgViewportPathname.split("/");
 
-      if (pathParts.length > 3 && pathParts[1] !== 'patterns' && pathParts[2] === 'patterns') {
-        addressReplacement += '&subsite=' + pathParts[1];
+      if (pathParts.length > 3 && pathParts[1] !== "patterns" && pathParts[2] === "patterns") {
+        addressReplacement += "&subsite=" + pathParts[1];
         href = sgViewportPathname.substr(1);
       } else {
         href = urlHandler.getFileName(pattern);
@@ -120,46 +120,46 @@
 
   /* Pattern Lab accordion dropdown */
   // Accordion dropdown
-  $('.fp-nav-container .sg-acc-handle').on("click", function(e){
+  $(".fp-nav-container .sg-acc-handle").on("click", function(e){
     e.preventDefault();
 
     var $this = $(this),
-      $panel = $this.next('.fp-nav-container .sg-acc-panel'),
-      subnav = $this.parent().parent().hasClass('sg-acc-panel');
+      $panel = $this.next(".fp-nav-container .sg-acc-panel"),
+      subnav = $this.parent().parent().hasClass("sg-acc-panel");
 
     //Close other panels if link isn't a subnavigation item
     if (!subnav) {
-      $('.fp-nav-container .sg-acc-handle').not($this).removeClass('active');
-      $('.fp-nav-container .sg-acc-panel').not($panel).removeClass('active');
+      $(".fp-nav-container .sg-acc-handle").not($this).removeClass("active");
+      $(".fp-nav-container .sg-acc-panel").not($panel).removeClass("active");
     }
 
     //Activate selected panel
-    $this.toggleClass('active');
-    $panel.toggleClass('active');
+    $this.toggleClass("active");
+    $panel.toggleClass("active");
     setAccordionHeight();
   });
 
   //Accordion Height
   function setAccordionHeight() {
-    var $activeAccordion = $('.fp-nav-container .sg-acc-panel.active').first(),
+    var $activeAccordion = $(".fp-nav-container .sg-acc-panel.active").first(),
       accordionHeight = $activeAccordion.height(),
       sh = $(document).height(), //Viewport Height
-      $headerHeight = $('.sg-header').height(),
+      $headerHeight = $(".sg-header").height(),
       availableHeight = sh-$headerHeight; //Screen height minus the height of the header
 
     $activeAccordion.height(availableHeight); //Set height of accordion to the available height
   }
 
-  $('.fp-nav-container .sg-nav-toggle').on("click", function(e){
+  $(".fp-nav-container .sg-nav-toggle").on("click", function(e){
     e.preventDefault();
-    $('.fp-nav-container .sg-nav-container').toggleClass('active');
+    $(".fp-nav-container .sg-nav-container").toggleClass("active");
   });
 
   // update the iframe with the source from clicked element in pull down menu. also close the menu
   // having it outside fixes an auto-close bug i ran into
   // replacing the default listener
-  $('.sg-nav a').not('.sg-acc-handle').off('click', '**');
-  $('.sg-nav a').not('.sg-acc-handle').on('click', function(e){
+  $(".sg-nav a").not(".sg-acc-handle").off("click", "**");
+  $(".sg-nav a").not(".sg-acc-handle").on("click", function(e){
 
     e.preventDefault();
 
@@ -171,24 +171,24 @@
 
     if (sgViewportPathname.match(/^\\/[^\\/]+\\/patterns/)) {
       if (navLinkHref.match(/^[^\\/]+\\/patterns/)) {
-        navLinkHref = navLinkHref.replace(/^[^\\/]+\\//, '');
+        navLinkHref = navLinkHref.replace(/^[^\\/]+\\//, "");
       } else {
-        navLinkHref = '../' + navLinkHref;
+        navLinkHref = "../" + navLinkHref;
       }
     }
 
     document.getElementById("sg-viewport").contentWindow.postMessage( { "path": navLinkHref }, targetOrigin);
 
     // close up the menu
-    $this.parents('.fp-nav-container .sg-acc-panel').toggleClass('active');
-    $this.parents('.fp-nav-container .sg-acc-panel').siblings('.sg-acc-handle').toggleClass('active');
+    $this.parents(".fp-nav-container .sg-acc-panel").toggleClass("active");
+    $this.parents(".fp-nav-container .sg-acc-panel").siblings(".sg-acc-handle").toggleClass("active");
 
     return false;
 
   });
 
   // Push viewport down beyond expanded toolbar.
-  document.getElementById('sg-vp-wrap').style.top = '` + (2.0625 + 2.0625 * subsites.length) + `em';
+  document.getElementById("sg-vp-wrap").style.top = "` + (2.0625 + 2.0625 * subsites.length) + `em";
 })();
 `;      // End backticked multi-line string.
         // /////////////////////////////////////////////////////////////////////
