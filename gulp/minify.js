32969b58ecbd64100b49643b3d5c4b88ef78bb53
e2tha-e <ee@electric-eloquence.io>
2016-05-22 23:21:05 -0400

    eslinting

--- gulp/minify.js
+++ gulp/minify.js
@@ -1,14 +1,12 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var conf = global.conf;
-  var gulp = require('gulp');
-  var plugins = require('gulp-load-plugins')();
+var conf = global.conf;
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 
-  gulp.task('minify:uglify', function () {
-    return gulp.src(conf.src + '/scripts/src/**/*.js')
-      .pipe(plugins.uglify())
-      .pipe(plugins.rename({extname: '.min.js'}))
-      .pipe(gulp.dest(conf.src + '/scripts/min'));
-  });
-})();
+gulp.task('minify:uglify', function () {
+  return gulp.src(conf.src + '/scripts/src/**/*.js')
+    .pipe(plugins.uglify())
+    .pipe(plugins.rename({extname: '.min.js'}))
+    .pipe(gulp.dest(conf.src + '/scripts/min'));
+});
