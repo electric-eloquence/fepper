f132e27d683059fd05678ecf3b534d87ffc8f3cc
e2tha-e <hyunhoee@gmail.com>
2016-06-15 07:19:42 -0400

    fp arguments can contain spaces

--- index.js
+++ index.js
@@ -3,12 +3,41 @@
 
 var cp = require('child_process');
 
-var utils = require('./core/lib/utils');
-
 // Set global.conf and process.env.CONF.
+var utils = require('./core/lib/utils');
 utils.conf();
+
+var argv = [];
+var i;
+var j = 1;
+var quoted = false;
+
 // Set up array of args for submission to Gulp.
-process.argv[2] = process.argv[2] ? process.argv[2] : 'default';
-var argv = process.argv.splice(2);
+argv[0] = process.argv[2] ? process.argv[2] : 'default';
+
+// Args containing spaces are wrapped in double quotes by the fp bash script.
+// Now, parse the process.argv array for those wrapped args, and create a new
+// array where they are formatted correctly.
+if (process.argv[3]) {
+  for (i = 3; i < process.argv.length; i++) {
+    if (quoted) {
+      argv[j] += ` ${process.argv[i]}`;
+    }
+    else {
+      argv[j] = process.argv[i];
+    }
+
+    if (argv[j][0] === '"') {
+      quoted = true;
+    }
+    else if (argv[j].slice(-1) === '"') {
+      quoted = false;
+    }
+
+    if (!quoted) {
+      j++;
+    }
+  }
+}
 
 cp.spawn('./node_modules/.bin/gulp', argv, {stdio: 'inherit'});
