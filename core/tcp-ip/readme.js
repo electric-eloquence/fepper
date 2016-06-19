f964cc9aefe503665629b83aee2e376c7d748220
e2tha-e <ee@electric-eloquence.io>
2016-06-19 09:29:31 -0400

    Squashed commit of the following:
    commit 1c5d0537a79bfe01e26be4383b6fd3e69a296c19
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Jun 19 09:28:42 2016 -0400
    
        extension convention update
    
    commit 14bb4fc8466ff8115b9126f48966a03d3db53bfc
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Jun 19 09:20:56 2016 -0400
    
        yaml convention update
    
    commit 9b79902cb4c7d8bb899d836a70d1b087f45ae8a7
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Jun 19 07:15:51 2016 -0400
    
        readme update
    
    commit 4c572c1ac3df7cd07d87663d6720951d2feeed54
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Jun 19 06:56:53 2016 -0400
    
        readme express page

--- core/tcp-ip/readme.js
+++ core/tcp-ip/readme.js
@@ -12,18 +12,11 @@ exports.main = function (req, res) {
   fs.readFile(__dirname + '/../../README.md', conf.enc, function (err, dat) {
     var converter = new pagedown.Converter();
     var htmlMd = converter.makeHtml(dat);
-    var output = '';
+    var output = htmlObj.head;
 
-    output += htmlObj.headWithMsg;
-    output += htmlObj.success;
     output += htmlMd + '\n';
-    output += '<p>&nbsp;</p>\n';
-    output += '<p>&nbsp;</p>\n';
     output += htmlObj.foot;
-    output = output.replace('{{ title }}', 'Installation success!');
-    output = output.replace('{{ msg_class }}', 'success');
-    output = output.replace('{{ message }}', '<h1>Installation success!</h1>');
-    output = output.replace(/localhost:3000/g, req.headers.host);
+    output = output.replace('{{ title }}', 'Fepper');
     res.end(output);
   });
 };
