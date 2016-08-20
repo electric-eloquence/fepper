a5ed7779282d1f1f98628651990ddeb38aef7e07
e2tha-e <ee@electric-eloquence.io>
2016-08-20 17:17:10 -0400

    Squashed commit of the following:
    commit 49835d99a6a01fb08b12fba50fdf55c3063d50bb
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 17:01:49 2016 -0400
    
        working viewalls
    
    commit b01147b9326403e48b010caf43df254d1e1295b5
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 16:26:21 2016 -0400
    
        type viewalls work
    
    commit c238ec14ca93c46faa82da598ef5fd8d715b7764
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 15:16:19 2016 -0400
    
        code cleanup
    
    commit 9dadb5404aff8c824808810536b3498065f954bf
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 13:28:08 2016 -0400
    
        viewalls in the right place
    
    commit 8ee041e34dd0014d78d1fe43df54c6ab79330be3
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 11:35:26 2016 -0400
    
        viewalls in the right place
    
    commit 165533e18473ff6616ab76fdccf59568ceb60c65
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 11:17:58 2016 -0400
    
        updating pseudopattern hunting for better ordering in nav
    
    commit 91d0fc3b458769e18130f7b7075ad9b1b10e1520
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 10:30:32 2016 -0400
    
        cleaned up
    
    commit 02c28dc598a3a5459a9f7ebb927277080e75c90b
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 10:22:49 2016 -0400
    
        working viewall for most types
    
    commit 663598c7dc218a0327340db84a32b35a4b2c2d4e
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Aug 20 08:51:06 2016 -0400
    
        viewall correctly at end
    
    commit 1e7b19aec375f5ac5a9a2f4fb908f43cd97dc1a6
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Aug 19 09:43:46 2016 -0400
    
        debugging
    
    commit 4bf43849acf7145f4eafdd82a22717917a5f7639
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Aug 19 09:31:21 2016 -0400
    
        eslintrc update
    
    commit a6099b099d68f2a35bed23ad63bddbf5901528ac
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Aug 19 09:28:23 2016 -0400
    
        minor ui updates
    
    commit bfb40435f86bc54afe0602985a68ff50243e9716
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Aug 19 08:53:36 2016 -0400
    
        skipping lint tasks for now
    
    commit 491fa6b367f98902cd77b6163ab3c36b6ee49802
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Aug 19 08:50:18 2016 -0400
    
        not cascading pattern states
    
    commit 0b04d86caaf76c3f80f23f278bdfe654cf39c0f7
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Aug 19 08:15:04 2016 -0400
    
        fixed lineage hunt

--- app/tasker.js
+++ app/tasker.js
@@ -163,7 +163,7 @@ gulp.task('restart', function (cb) {
 gulp.task('static', function (cb) {
   runSequence(
     ['contrib:static:preprocess', 'custom:static:preprocess'],
-    'lint',
+//    'lint',
     'minify',
     'fepper:static-generate',
     ['contrib:static', 'custom:static'],
@@ -174,7 +174,7 @@ gulp.task('static', function (cb) {
 gulp.task('syncback', function (cb) {
   runSequence(
     ['contrib:syncback:preprocess', 'custom:syncback:preprocess'],
-    'lint',
+//    'lint',
     'minify',
     'frontend-copy',
     'template',
