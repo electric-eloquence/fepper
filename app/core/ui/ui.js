6e9ea554af47b4768812181a8d4b893adcbcf569
e2tha-e <ee@electric-eloquence.io>
2016-09-27 06:31:02 -0400

    Squashed commit of the following:
    commit 53f9579319c95194d627418ad904ee9dd946e65e
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Tue Sep 27 06:24:50 2016 -0400
    
        viewall links work
    
    commit a423609395df0955ce61e7f40fe350a0c7465d82
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Sep 26 23:15:01 2016 -0400
    
        subtype links on all viewall page (styleguide.html)
    
    commit bea44d52aac7d2a8086aabeb4b0a9df89da3bba8
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Sep 26 20:45:00 2016 -0400
    
        viewall links work
    
    commit 2e8f252ec5445d72bda618873f26a91094bdca67
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Sep 26 09:55:34 2016 -0400
    
        eslinting ui.js
    
    commit f8dc6c07d4b131e9d7ac78e8cf6b7310a57e56ea
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Sep 26 09:54:16 2016 -0400
    
        showing progess on viewall pages
    
    commit ee75f9720128754fe1de8f10f6c05a1be25c21af
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Sep 25 14:30:11 2016 -0400
    
        deleting unused js
    
    commit 037e38accced3282f0f94caf9bee5a1df61d2247
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Sep 25 14:19:38 2016 -0400
    
        reinstate defaultPattern as a navigable pattern
    
    commit e109c16e5e91eec70d89ef47e71ebd6b4060c151
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Sep 25 14:11:05 2016 -0400
    
        reinstate defaultPattern as a navigable pattern
    
    commit 213d9a712d6ee85736ba0fdc974edc35f060b3a9
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Sep 25 13:29:13 2016 -0400
    
        removing unnecessary minified files
    
    commit 7f4e20646ffb878c4a0a0fc32864e750ee0087d8
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Sep 24 11:23:00 2016 -0400
    
        hard-coding title

--- app/core/ui/ui.js
+++ app/core/ui/ui.js
@@ -10,21 +10,10 @@ var srcDir = conf.ui.paths.source;
 
 module.exports = class {
   build(arg) {
-    var indexFile;
-    var indexFileOut;
     var patternlab = require(utils.pathResolve(conf.ui.paths.core.lib + '/patternlab.js'))(conf.ui);
 
     if (typeof arg === 'undefined') {
       patternlab.build(function () {});
-
-      // Some overrides after the stock build.
-      indexFile = utils.pathResolve(pubDir.root + '/index.html');
-      indexFileOut = fs.readFileSync(indexFile, conf.enc);
-      indexFileOut = indexFileOut.replace(/(allow-same-origin allow-scripts)/, '$1 allow-forms allow-popups');
-      indexFileOut = indexFileOut.replace(/(<\/body>)/, '    <script src="_scripts/src/variables.styl" type="text/javascript"></script>\n$1');
-      indexFileOut = indexFileOut.replace(/(<\/body>)/, '    <script src="_scripts/src/fepper-obj.js"></script>\n$1');
-      indexFileOut = indexFileOut.replace(/(<\/body>)/, '    <script src="_scripts/patternlab-overrider.js"></script>\n$1');
-      fs.writeFileSync(indexFile, indexFileOut);
     }
     else if (arg === 'v') {
       patternlab.version();
