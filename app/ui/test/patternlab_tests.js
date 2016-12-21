4a42df524bd422331b13f5d97620eb83213db411
e2tha-e <ee@electric-eloquence.io>
2016-12-21 10:51:38 -0500

    resolving patternlab paths

--- app/ui/test/patternlab_tests.js
+++ app/ui/test/patternlab_tests.js
@@ -4,7 +4,7 @@ exports.patternlab_test = {
   'buildPatternData - should merge all JSON files in the data folder except listitems': function (test) {
     var fs = require('fs-extra');
     var plMain = require('../core/lib/patternlab');
-    var data_dir = './test/files/_data/';
+    var data_dir = './test/files/_data';
 
     var dataResult = plMain.buildPatternData(data_dir, fs);
     test.equals(dataResult.data, 'test');
