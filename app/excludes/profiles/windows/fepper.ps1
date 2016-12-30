7802b3b27768dd6e7944de7e09eaf301eafe1b6b
e2tha-e <ee@electric-eloquence.io>
2016-12-30 10:06:50 -0500

    using path.resolve to get cp commands to work in windows

--- app/excludes/profiles/windows/fepper.ps1
+++ app/excludes/profiles/windows/fepper.ps1
@@ -9,42 +9,10 @@ if ($whereNode -Like "*\node.exe")
     rm node-v6.9.2-x64.msi
   }
 
-  if (
-    -Not (Test-Path conf.yml) -or
-    -Not (Test-Path extend) -or
-    -Not (Test-Path node_modules) -or
-    -Not (Test-Path patternlab-config.json) -or
-    -Not (Test-Path pref.yml))
+  if (-Not (Test-Path node_modules))
   {
-    if (-Not (Test-Path conf.yml))
-    {
-      cp app\excludes\conf.yml .
-    }
-
-    if (-Not (Test-Path extend))
-    {
-      cp -Recurse app\excludes\extend .
-    }
-
-    if (-Not (Test-Path node_modules))
-    {
-      echo "npm installing..."
-      npm install
-    }
-
-    if (-Not (Test-Path patternlab-config.json))
-    {
-      cp app\excludes\patternlab-config.json .
-      if (-Not (Test-Path source))
-      {
-        cp -Recurse app\excludes\profiles\main\source .
-      }
-    }
-
-    if (-Not (Test-Path pref.yml))
-    {
-      cp app\excludes\pref.yml .
-    }
+    echo "npm installing..."
+    npm install
   }
 
   $argList = $args[0]
