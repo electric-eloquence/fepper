7cedb9a9bb9cdb1600ded5c7244718857951dbe7
e2tha-e <ee@electric-eloquence.io>
2017-10-14 09:15:57 -0400

    test description updates

--- test/install-tests.js
+++ test/install-tests.js
@@ -4,39 +4,39 @@ const expect = require('chai').expect;
 const fs = require('fs-extra');
 
 describe('Main Installer', function () {
-  it('should copy conf.yml to the root directory', function () {
+  it('should copy conf.yml to /', function () {
     expect(fs.existsSync('conf.yml')).to.equal(true);
   });
 
-  it('should copy patternlab-config.json to the root directory', function () {
+  it('should copy patternlab-config.json to /', function () {
     expect(fs.existsSync('patternlab-config.json')).to.equal(true);
   });
 
-  it('should complete with pref.yml in the root directory', function () {
+  it('should complete with pref.yml in /', function () {
     expect(fs.existsSync('pref.yml')).to.equal(true);
   });
 
-  it('should complete with source in the root directory', function () {
+  it('should complete with source in /', function () {
     expect(fs.existsSync('source')).to.equal(true);
   });
 
-  it('should copy extend to the root directory', function () {
+  it('should copy extend to /', function () {
     expect(fs.existsSync('extend')).to.equal(true);
   });
 
-  it('should install npms in the extend directory', function () {
+  it('should install npms in /extend', function () {
     expect(fs.existsSync('extend/node_modules')).to.equal(true);
   });
 
-  it('should install npms in the public directory', function () {
+  it('should install npms in /public', function () {
     expect(fs.existsSync('public/node_modules')).to.equal(true);
   });
 
-  it('should compile index.mustache to node_modules/fepper/ui/core/styleguide/', function () {
+  it('should compile index.mustache to /node_modules/fepper/ui/core/styleguide/', function () {
     expect(fs.existsSync('node_modules/fepper/ui/core/styleguide/index.mustache')).to.equal(true);
   });
 
-  it('should compile styleguide.html to public/node_modules/fepper-ui/', function () {
+  it('should compile styleguide.html to /public/node_modules/fepper-ui/', function () {
     expect(fs.existsSync('public/node_modules/fepper-ui/styleguide.html')).to.equal(true);
   });
 });
