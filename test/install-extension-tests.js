9edeb66961b9124fd52a14ec72643614de2a7310
e2tha-e <ee@electric-eloquence.io>
2018-10-20 16:37:23 -0400

    updating test description to match other descriptions

--- test/install-extension-tests.js
+++ test/install-extension-tests.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const fs = require('fs-extra');
 
 describe('Extension Installer', function () {
-  it('should install npms in /extend', function () {
+  it('should install npms in /extend/', function () {
     expect(fs.existsSync('extend/node_modules')).to.equal(true);
   });
 });
