1471c3c590cc766e0492c350bd4a30856ca1b40a
e2tha-e <ee@electric-eloquence.io>
2019-03-17 09:14:35 -0400

    test updates

--- test/install-tests.js
+++ test/install-tests.js
@@ -5,30 +5,30 @@ const fs = require('fs-extra');
 
 describe('Installer', function () {
   it('should copy conf.yml to /', function () {
-    expect(fs.existsSync('conf.yml')).to.equal(true);
+    expect(fs.existsSync('conf.yml')).to.be.true;
   });
 
   it('should copy patternlab-config.json to /', function () {
-    expect(fs.existsSync('patternlab-config.json')).to.equal(true);
+    expect(fs.existsSync('patternlab-config.json')).to.be.true;
   });
 
   it('should complete with pref.yml in /', function () {
-    expect(fs.existsSync('pref.yml')).to.equal(true);
+    expect(fs.existsSync('pref.yml')).to.be.true;
   });
 
   it('should complete with source in /', function () {
-    expect(fs.existsSync('source')).to.equal(true);
+    expect(fs.existsSync('source')).to.be.true;
   });
 
   it('should copy extend to /', function () {
-    expect(fs.existsSync('extend')).to.equal(true);
+    expect(fs.existsSync('extend')).to.be.true;
   });
 
   it('should install npms in /public/', function () {
-    expect(fs.existsSync('public/node_modules')).to.equal(true);
+    expect(fs.existsSync('public/node_modules')).to.be.true;
   });
 
   it('should compile index.html to /public/', function () {
-    expect(fs.existsSync('public/index.html')).to.equal(true);
+    expect(fs.existsSync('public/index.html')).to.be.true;
   });
 });
