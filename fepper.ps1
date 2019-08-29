c31d722b64b116b1d3a97435b0e89e917ec4b3eb
e2tha-e <ee@electric-eloquence.io>
2019-08-29 11:16:34 -0400

    updated recommended and default node versions

--- fepper.ps1
+++ fepper.ps1
@@ -11,7 +11,7 @@ else
   $archMsi = "x64.msi"
 }
 
-$nodeVersion = "v10.15.3"
+$nodeVersion = "v12.9.1"
 $nodeMsi = "node-" + $nodeVersion + "-" + $archMsi
 $nodeMsiFull = $PSScriptRoot + "\" + $nodeMsi
 $whereNode = where.exe node
