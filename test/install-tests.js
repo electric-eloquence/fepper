6d32db070510b701fe9dce40b3c300a28af10d27
e2tha-e <ee@electric-eloquence.io>
2019-08-29 12:27:28 -0400

    replaced "should" wording from test descriptions

--- test/install-tests.js
+++ test/install-tests.js
@@ -4,31 +4,31 @@ const {expect} = require('chai');
 const fs = require('fs-extra');
 
 describe('Installer', function () {
-  it('should copy conf.yml to /', function () {
+  it('copies conf.yml to /', function () {
     expect(fs.existsSync('conf.yml')).to.be.true;
   });
 
-  it('should copy patternlab-config.json to /', function () {
+  it('copies patternlab-config.json to /', function () {
     expect(fs.existsSync('patternlab-config.json')).to.be.true;
   });
 
-  it('should complete with pref.yml in /', function () {
+  it('completes with pref.yml in /', function () {
     expect(fs.existsSync('pref.yml')).to.be.true;
   });
 
-  it('should complete with source in /', function () {
+  it('completes with source in /', function () {
     expect(fs.existsSync('source')).to.be.true;
   });
 
-  it('should copy extend to /', function () {
+  it('copies extend to /', function () {
     expect(fs.existsSync('extend')).to.be.true;
   });
 
-  it('should install npms in /public/', function () {
+  it('installs npms in /public/', function () {
     expect(fs.existsSync('public/node_modules')).to.be.true;
   });
 
-  it('should compile index.html to /public/', function () {
+  it('compiles index.html to /public/', function () {
     expect(fs.existsSync('public/index.html')).to.be.true;
   });
 });
