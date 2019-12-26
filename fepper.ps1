567cfcfe4a507017a4beeb67766dc37d4365f0c8
e2tha-e <ee@electric-eloquence.io>
2019-12-26 16:39:26 -0500

    bumped packaged nodejs version

--- fepper.ps1
+++ fepper.ps1
@@ -11,7 +11,7 @@ else
   $archMsi = "x64.msi"
 }
 
-$nodeVersion = "v12.11.0"
+$nodeVersion = "v12.14.0"
 $nodeMsi = "node-" + $nodeVersion + "-" + $archMsi
 $nodeMsiFull = $PSScriptRoot + "\" + $nodeMsi
 $whereNode = where.exe node
